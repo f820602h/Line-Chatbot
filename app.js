187400b4743f669b65ee714513c5074130a5577a
max.lee[李炳寬] <f820602h@yahoo.com.tw>
2020-04-20 17:21:21 +0800

    make it better

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ var bot = linebot({
 
 // 當有人傳送訊息給Bot時
 bot.on('message', function (event) {
-  // event.message.text是使用者傳給bot的訊息
-  // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
   event.reply(event.message.text).then(function (data) {
-    // 當訊息成功回傳後的處理
+    console.log(event.message.text)
   }).catch(function (error) {
-    // 當訊息回傳失敗後的處理
+    console.log(error)
   });
 });
 
